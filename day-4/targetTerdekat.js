9dea428032f558067e16eeb6790925f6556abfea
Muhammad Rinaldi <muhammad.rinaldi007.mr@gmail.com>
2024-02-13 10:19:42 +0800

    Mencari Target Terdekat

--- day-4/targetTerdekat.js
+++ day-4/targetTerdekat.js
@@ -23,23 +23,9 @@ function targetTerdekat(target) {
         }
     }
     return jarak;
-
 }
 console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])); //3
 console.log(targetTerdekat(['o', ' ', ' ', ' ', 'x', 'x', 'x'])); // 4
 console.log(targetTerdekat(['x', ' ', ' ', ' ', 'x', 'x', 'o', ' '])); // 1
 console.log(targetTerdekat([' ', ' ', 'o', ' '])); // 0
 console.log(targetTerdekat([' ', 'o', ' ', 'x', 'x', ' ', ' ', 'x'])); // 2
-
-
-
-
-
-
-
-// // TEST CASES
-// console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])); // 3
-// console.log(targetTerdekat(['o', ' ', ' ', ' ', 'x', 'x', 'x'])); // 4
-// console.log(targetTerdekat(['x', ' ', ' ', ' ', 'x', 'x', 'o', ' '])); // 1
-// console.log(targetTerdekat([' ', ' ', 'o', ' '])); // 0
-// console.log(targetTerdekat([' ', 'o', ' ', 'x', 'x', ' ', ' ', 'x'])); // 2
